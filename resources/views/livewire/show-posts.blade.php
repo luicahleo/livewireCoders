9895006f08cf555433175e4a850f4efcef6293ec
luicahleo <luisrolandocahuanaleon@gmail.com>
2021-11-09 23:38:28 +0100

    terminado buscador

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -1,37 +1,47 @@
 <div>
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
-    {{$search}}
-
-
-    <div class="container">Lista de Posts</div>
-    <div class="container">
-        <input type="text" wire:model='search'>
+    <div class="container mb-4">
+        <x-jet-input type="text" wire:model="search" placeholder="Buscador" />
     </div>
-    <div class="container">
-        <table class="table table-bordered border-primary">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Titulo</th>
-                    <th scope="col">Contenido</th>
-                </tr>
-            </thead>
 
-            <tbody>
-                @foreach ($posts as $post)
+    @if ($posts->count())
+
+        <div class="container">Lista de Posts</div>
+
+        <div class="container">
+            <table class="table table-bordered border-primary">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Titulo</th>
+                        <th scope="col">Contenido</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach ($posts as $post)
 
                     <tr>
                         <th scope="row">{{$post->id}}</th>
                         <td>{{$post->title}}</td>
                         <td>{{$post->content}}</td>
                     </tr>
-                @endforeach
+                    @endforeach
 
-            </tbody>
-        </table>
-        {{-- <div>
-            {{$posts->links()}}
-        </div> --}}
+                </tbody>
+            </table>
+            {{-- <div>
+                {{$posts->links()}}
+            </div> --}}
+        </div>
+        
+    @else
+
+    <div class="container">
+        No hay registros que mostrar
     </div>
+        
+    @endif
+
 
 </div>
