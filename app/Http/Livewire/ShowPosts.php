2a5f97ce9385b6a5c35c10d64e4b16fd1a3a405c
luicahleo <luisrolandocahuanaleon@gmail.com>
2021-11-09 11:37:56 +0100

    arreglado pagination

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -5,8 +5,12 @@ namespace App\Http\Livewire;
 use App\Models\Post;
 use Livewire\Component;
 
+use Livewire\WithPagination;
 class ShowPosts extends Component
 {
+
+use WithPagination;
+
     public function render()
     {
 
