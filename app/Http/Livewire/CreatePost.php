d7bf4bd32761bbc7e7bfdeb427ea2fa0872e224c
luicahleo <luisrolandocahuanaleon@gmail.com>
2022-01-26 22:56:19 +0100

    Merge branch 'main' of https://github.com/luicahleo/livewirecoders

--- app/Http/Livewire/CreatePost.php
+++ app/Http/Livewire/CreatePost.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
 
 class CreatePost extends Component
 {
+
+    public $open = false;
+
+    public $title, $content;
+
+    public function save(){
+        Post::create([
+            'title' => $this->title,
+            'content' => $this->content
+        ]);
+    }
+
+
     public function render()
     {
         return view('livewire.create-post');
--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,6 +1,32 @@
 <div>
 
-    <x-jet-danger-button>
+    <x-jet-danger-button wire:click="$set('open', true)">
         Crear nuevo post
     </x-jet-danger-button>
+
+    {{-- usaremos el modal dado por jetsream --}}
+    <x-jet-dialog-modal wire:model="open">
+        <x-slot name="title">
+            Crear nuevo post
+        </x-slot>
+        <x-slot name="content">
+            <div class="mb-4">
+                <x-jet-label value="Titulo del post" />
+                {{-- wire:model.defer solamente actuara cuando desencadenemos una accion --}}
+                <x-jet-input type="text" class="w-full"  wire:model.defer="title"/>
+            </div>
+            <div class="mb-4">
+                {{-- este text area es bootstrap --}}
+                <div class="form-group">
+                    <label for="textArea">Contenido del post</label>
+                    <textarea wire:model.defer="content" class="form-control rounded-0" id="textArea" rows="3"></textarea>
+                </div>
+            </div>
+        </x-slot>
+        <x-slot name="footer">
+            <x-jet-secondary-button >Cacelar</x-jet-secondary-button>
+            <x-jet-danger-button wire:click='save'>Crear post</x-jet-danger-button>
+        </x-slot>
+    </x-jet-dialog-modal>
+
 </div>
